6762bd1b67889a9710f065fdc5b7bd5cb6006cdb
Merce5 <mercealomar@gmail.com>
2026-04-25 11:07:46 +0200

    Creo el modelo de incidencia

---
+++ com.uem.arbolbinario/src/main/java/domain/Incidencia.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Incidencia {
+
+    private final String nombre;
+    private final int codigo;
+
+    public Incidencia(String nombre, int codigo) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+}
